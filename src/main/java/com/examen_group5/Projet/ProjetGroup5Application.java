c79b053a54d7315a563597e19e9e83ca95104a71
Souleymane Diagne <s.d84@zig.univ.sn>
2023-10-31 19:51:49 +0000

    Mise en ligne

---
+++ src/main/java/com/examen_group5/Projet/ProjetGroup5Application.java
@@ -0,0 +1,13 @@
+package com.examen_group5.Projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetGroup5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetGroup5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen_group5/Projet/ProjetGroup5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen_group5.Projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetGroup5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
